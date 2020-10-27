cd86800651cb255eaf2806ddaa101a9678116c2a
BoHuang2018 <tigerhuangbo@icloud.com>
2020-10-27 14:04:17 +0100

    delete configuration file by gitignore

--- chapter_02/monthly_flights_data_collecter/gcp_configuration.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PROJECT=$(gcloud config get-value project)
-BUCKET=intense-howl-274214-demos-ml
-REGION=us-central1
-CLOUD_FUNCTION=flights_data_collect50lZZonhqYs7NDQueUIDMLN6B9HIWa0N
-TOKEN_RUN_CLOUD_FUNCTION=BwbMjrbWr5YlYWirpwOcQk3UL2wGgyQ8
