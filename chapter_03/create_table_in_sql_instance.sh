b5e773e43544ce27d7cb6b01338a7c4ff0ac944f
BoHuang2018 <tigerhuangbo@icloud.com>
2020-11-12 21:13:55 +0100

    added cloud_sql_configuration.sh

--- chapter_03/create_table_in_sql_instance.sh
+++ chapter_03/create_table_in_sql_instance.sh
@@ -5,4 +5,4 @@ bash authorize_cloudshell_by_patching_authorized_networkds.sh
 
 # Connect to MySQL using its IP address and do the import
 sql_public_ip_address=$(gcloud sql instances describe ${name_of_instance} --format="value(ipAddresses.ipAddress)")
-mysql --host=$sql_public_ip_address --user=root --password=${root_password_of_instance} --verbose < create_table.sql
+mysql --host=$sql_public_ip_address --user=root --password=${root_password_of_instance} --verbose < create_table_in_sql_instance.sql
